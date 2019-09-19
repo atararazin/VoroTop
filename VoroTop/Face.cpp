09ec03ed53f75232f80b60715764a2bde2b392c0
Atara Razin <atararazin@gmail.com>
2019-09-19 19:58:17 +0300

    added Face c and h files

---
+++ VoroTop/Face.cpp
@@ -0,0 +1,9 @@
+//
+// Created by atara on 9/19/19.
+//
+
+#include "Face.h"
+
+long Face::addNum(int num) {
+    this->edges.push_back(num);
+}
---
+++ VoroTop/Face.h
@@ -0,0 +1,18 @@
+//
+// Created by atara on 9/19/19.
+//
+
+#ifndef VOROTOP_FACE_H
+#define VOROTOP_FACE_H
+using namespace std;
+
+class Face {
+private:
+    vector<int> edges;
+public:
+    addNum(int);
+
+};
+
+
+#endif //VOROTOP_FACE_H
